a5df71523a422f1b9f5ab75f2aad587e7b2da61d
deukey <deukey@naver.com>
2014-08-14 17:09:25 +0900

    last
    last

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,20 +1,25 @@
-from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
-    # Request the context of the request.
-    # The context contains information such as the client's machine details, for example.
     context = RequestContext(request)
 
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
     return render_to_response('rango/index.html', context_dict, context)
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+
+    category_name = category_name_url.replace('_', ' ')
+    context_dict = {'category_name': category_name}
+
+    try:
+        category = Category.objects.get(name=category_name)
+
 
 def about(request):
     context = RequestContext(request, {'welcomemessage': 'Welcome!'})
